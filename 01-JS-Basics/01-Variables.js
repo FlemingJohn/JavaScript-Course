238d47f3216bc073bff4045c1ca8ea4e449f9a92
Fleming John <flemjohn08@gmail.com>
2025-08-10 11:52:25 +0530

    Initial commit

---
+++ 01-JS-Basics/01-Variables.js
@@ -0,0 +1,25 @@
+// variables - used to store values 
+// types - var, let and const 
+// var and cont - immutable
+// let - mutable 
+
+// defining the variables
+var x = 10;
+let y = 5;
+const z = 15;
+
+// printing 
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+// creating the object in const 
+const person = {
+    name: "Fleming",
+    age: 21
+}
+
+// changing the values of the object 
+person.name = "John";
+console.log(person);
