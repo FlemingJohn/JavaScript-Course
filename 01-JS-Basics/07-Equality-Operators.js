7cd9a105a14c67b83a64d51fd59793f31e546806
Fleming John <flemjohn08@gmail.com>
2025-08-20 11:47:43 +0530

    finsihed equlity operator

---
+++ 01-JS-Basics/07-Equality-Operators.js
@@ -0,0 +1,9 @@
+// equlity operator checks whether two values is equal or not 
+// operator -> ==
+
+console.log(42 == '42'); //returns true beacause it automatically converts it into string 
+
+// triple equlity operator 
+// operator -> ===
+
+console.log(42 === '42'); //returns false because it is doesnt converts instead check the left and right operand are equal 
