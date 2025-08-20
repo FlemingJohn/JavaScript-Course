79e24f8288dfa800cd66f5aac7d72f9052efadb2
Fleming John <flemjohn08@gmail.com>
2025-08-20 09:51:03 +0530

    finished coercion

---
+++ 01-JS-Basics/05-Type-Coercion.js
@@ -0,0 +1,12 @@
+// coercion - conversion of one datatype to another datatype
+
+
+// print this in console 
+42 + '' //stirng 
+
+42 + '9' // concatenate and gives as string  '490'
+
+
+42 - '7' //does mathematical calculation and give it as value converts to number or integer
+
+console.log(typeof(42 - '7'));
